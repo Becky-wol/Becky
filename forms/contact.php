6a3a51940e71c1f4ad3728a63a57115d0821a586
stayathomelibrary <65277392+Becky-wol@users.noreply.github.com>
2021-05-01 16:51:35 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -19,9 +19,9 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->from_name = $_GET['name'];
+  $contact->from_email = $_GET['email'];
+  $contact->subject = $_GET['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
